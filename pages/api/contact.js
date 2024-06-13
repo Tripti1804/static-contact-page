dc17b5097ec96f1a9c73c805c589e3ab5ef0c787
Tripti1804 <cutetriptisharma1804@gmail.com>
2024-06-13 12:14:45 +0530

    Text color change

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,13 +1,13 @@
 // pages/api/contact.js
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const { name, email,contact, message } = req.body;
+  if (req.method === 'POST') {
+      const { name, email,contact, message } = req.body;
 
-        // Here you would typically handle the form data, e.g., save it to a database or send an email
-        console.log('Received contact form submission:', { name, email,contact, message });
+      // Here you would typically handle the form data, e.g., save it to a database or send an email
+      console.log('Received contact form submission:', { name, email,contact, message });
 
-        res.status(200).json({ success: true });
-    } else {
-        res.status(405).json({ error: 'Method not allowed' });
-    }
+      res.status(200).json({ success: true });
+  } else {
+      res.status(405).json({ error: 'Method not allowed' });
+  }
 }
